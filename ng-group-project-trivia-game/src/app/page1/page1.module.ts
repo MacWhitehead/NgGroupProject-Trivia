0ebe51d11168385d92a3dc1cee7344505d968424
Terran Adams <terranadams@Terrans-MacBook-Pro.local>
2021-04-21 08:35:39 -0600

    log in and sign up console.logged

--- ng-group-project-trivia-game/src/app/page1/page1.module.ts
+++ ng-group-project-trivia-game/src/app/page1/page1.module.ts
@@ -6,10 +6,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     MatButtonModule,
--- ng-group-project-trivia-game/src/app/services/auth.service.ts
+++ ng-group-project-trivia-game/src/app/services/auth.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-// import { auth } from 'firebase/app'; THIS DOESN"T WORK ANYMORE
 import firebase from 'firebase/app';
-// then use the full path to get the auth service.
-// firebase.auth.
-import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore'; 
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { User } from '../interfaces/user';
+import { AngularFireAuth } from "@angular/fire/auth";
 
-@Injectable({providedIn: 'root'})
+@Injectable({
+  providedIn: 'root'
+})
+
 export class AuthService {
-  user$: Observable<User>;
 
   constructor(
-    private afAuth: AngularFireAuth,
-    private afs: AngularFirestore,
-    private router: Router
-  ) {
+    public afAuth: AngularFireAuth, // Inject Firebase auth service
+  ) { }
+
+  // Sign in with Google
+  GoogleAuthLogin() {
+    return this.AuthLogin(new firebase.auth.GoogleAuthProvider());
+  }  
+
+  GoogleAuthSignup() {
+    return this.AuthSignup(new firebase.auth.GoogleAuthProvider());
   }
 
+  // Auth logic to run auth providers
+  AuthLogin(provider) {
+    return this.afAuth.signInWithPopup(provider)
+    .then((result) => {
+        console.log('You have been successfully logged in!')
+    }).catch((error) => {
+        console.log(error)
+    })
+  }
 
+  AuthSignup(provider) {
+    return this.afAuth.signInWithPopup(provider)
+    .then((result) => {
+        console.log('You have been successfully signed up!')
+    }).catch((error) => {
+        console.log(error)
+    })
+  }
 
 }
