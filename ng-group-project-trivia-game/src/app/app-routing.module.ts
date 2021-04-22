c525fd37fcb6996017a7b9a57f84121f833e8485
MacWhitehead <mackayla.whitehead7485@stu.mtec.edu>
2021-04-22 08:46:37 -0600

    Added some styling + percentages

--- ng-group-project-trivia-game/src/app/app-routing.module.ts
+++ ng-group-project-trivia-game/src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
   {path: "user-details", component: UserDetailsComponent},
-=======
-  {path: "user-details", component: UserDetailsComponent}
->>>>>>> 6c751c693aeb40559c0406ff6b96b5ca16916851
 ];
 
 @NgModule({
--- ng-group-project-trivia-game/src/app/app.module.ts
+++ ng-group-project-trivia-game/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TriviaPageComponent } from './components/trivia-page/trivia-page.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
-<<<<<<< HEAD
 
 //Mats
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -17,18 +16,12 @@ import {MatRadioModule} from '@angular/material/radio';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
-=======
->>>>>>> 6c751c693aeb40559c0406ff6b96b5ca16916851
 
 @NgModule({
   declarations: [
     AppComponent,
     TriviaPageComponent,
-<<<<<<< HEAD
     UserDetailsComponent,
-=======
-    UserDetailsComponent
->>>>>>> 6c751c693aeb40559c0406ff6b96b5ca16916851
   ],
   imports: [
     BrowserModule,
