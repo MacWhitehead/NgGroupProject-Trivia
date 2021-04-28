f2f93bc2b026db7aa135d4e53cd549fe67796e67
Samuel Norman <samuel.norman7103@stu.mtec.edu>
2021-04-28 07:57:32 -0600

    resolving conlficts

--- ng-group-project-trivia-game/src/app/app.module.ts
+++ ng-group-project-trivia-game/src/app/app.module.ts
@@ -10,12 +10,9 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TriviaPageComponent } from './components/trivia-page/trivia-page.component';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 
-=======
 import { UserDetailsComponent } from './components/user-details/user-details.component';
->>>>>>> dd67f27fa5d59cb3e5d07ce57737f7ce16a10914
 
 //Mats
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -30,33 +27,25 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import {MatTooltipModule} from '@angular/material/tooltip';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { MatButtonModule } from '@angular/material/button';
-=======
+
 import {MatTableModule} from '@angular/material/table';
 import { CreateGameComponent } from './components/create-game/create-game.component';
 import { environment } from 'src/environments/environment';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
->>>>>>> dd67f27fa5d59cb3e5d07ce57737f7ce16a10914
 
 @NgModule({
   declarations: [
     AppComponent,
     TriviaPageComponent,
-<<<<<<< HEAD
-=======
     UserDetailsComponent,
     CreateGameComponent,
     NavBarComponent
->>>>>>> dd67f27fa5d59cb3e5d07ce57737f7ce16a10914
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
@@ -66,9 +55,7 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     MatRadioModule, 
     MatProgressSpinnerModule,
     MatButtonModule,
-    HttpClientModule
-=======
-    BrowserAnimationsModule, 
+    HttpClientModule, 
     MatToolbarModule,
     MatCardModule,
     MatGridListModule,
@@ -96,7 +83,6 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     AngularFireAuthModule,
     MatTooltipModule
 
->>>>>>> dd67f27fa5d59cb3e5d07ce57737f7ce16a10914
   ],
   bootstrap: [AppComponent]
 })
